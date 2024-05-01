2091bbd54fd8e052423443c25ab92b9b44426447
aldm <d44.a1@yandex.ru>
2024-05-02 00:16:53 +0300

    1

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ const swiper = new Swiper(".tours__swiper", {
   loop: false,
   slidesPerView: 3,
   spaceBetween: 40,
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
