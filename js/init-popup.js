da27c10073bc3ae5d9a4be6b769ba52734a3b54b
aldm <d44.a1@yandex.ru>
2024-05-04 04:02:50 +0300

    1

--- js/init-popup.js
+++ js/init-popup.js
@@ -1,10 +1,12 @@
 //-------------------------------------
 const popupPage = document.querySelector(".popup");
-const popupClose = document.querySelector(".popup-close");
+const popupForm = document.querySelector(".popup-form");
+const popupClose = document.querySelectorAll(".popup-close");
 const popupMainImage = document.querySelector(".element-m-img");
 const popupTitle = document.querySelector(".element-title");
 const popupParagraph = document.querySelector(".element-paragraph");
 const popupPrice = document.querySelector(".element-price");
+const buttonSubmit = document.querySelectorAll(".general-button");
 
 document.addEventListener("DOMContentLoaded", function () {
   const elementsWithClass = document.querySelectorAll(".element");
@@ -27,6 +29,18 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-popupClose.addEventListener("click", () => {
-  popupPage.classList.remove("popup-open");
+popupClose.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    event.stopPropagation();
+    popupPage.classList.remove("popup-open");
+    popupForm.classList.remove("popup-form-open");
+  });
+});
+
+buttonSubmit.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    event.stopPropagation();
+    popupPage.classList.remove("popup-open");
+    popupForm.classList.add("popup-form-open");
+  });
 });
